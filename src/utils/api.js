b1a38b16242903174fb7cb2410bb035126264193
nikhilkumar50 <nikhilkumarray9889@gmail.com>
2023-07-31 00:36:47 +0530

    commit

---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":'58899d2161msh60d45d4cf50fc2fp106664jsnf83cee137068',
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
